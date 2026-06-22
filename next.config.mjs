41dec570bdff1d6cbbfefe30a18db1c1aa5f1092
Ghassen Jemiai <hiallnigas@gmail.com>
2026-06-22 14:30:04 +0200

    fix pipeline

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,14 @@
 /** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === 'production';
-const repoName = '/beamerboi.github.io';
+const repositoryName = process.env.GITHUB_REPOSITORY?.split('/')[1] ?? '';
+const isGitHubPages = process.env.GITHUB_ACTIONS === 'true';
+const isProjectPages = isGitHubPages && !repositoryName.endsWith('.github.io');
+const basePath = isProjectPages ? `/${repositoryName}` : '';
 
 const nextConfig = {
   output: 'export',
   images: { unoptimized: true },
-  basePath: isProd ? repoName : '',
-  assetPrefix: isProd ? repoName : '',
+  basePath,
+  assetPrefix: basePath,
 };
 
 export default nextConfig;
